eaabc760748c6d804c9ca48a44f45f210061b54a
Judhunja <judxhunja@gmail.com>
2024-05-29 21:56:33 +0300

    registering users with email and password after hashing their passwords

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -3,10 +3,31 @@
 
 
 import bcrypt
+from db import DB
+from db import User
+from sqlalchemy.exc import NoResultFound
 
 
-def _hash_password(password: str) -> bytes:
-    """Returns a salted hash of the input password"""
-    salt = bcrypt.gensalt()
+class Auth:
+    """Auth class to interact with the authentication database.
+    """
 
-    return bcrypt.hashpw(password.encode(), salt)
+    def __init__(self):
+        self._db = DB()
+
+    def _hash_password(self, password: str) -> bytes:
+        """Returns a salted hash of the input password"""
+        salt = bcrypt.gensalt()
+
+        return bcrypt.hashpw(password.encode(), salt)
+
+    def register_user(self, email: str, password: str) -> User:
+        """Authenticates a user"""
+        try:
+            user_exists = self._db.find_user_by(email=email)
+            if user_exists:
+                raise ValueError(f"User {email} already exists")
+        except NoResultFound:
+            password = self._hash_password(password).decode()
+            user = self._db.add_user(email, password)
+            return user
