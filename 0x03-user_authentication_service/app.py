cf651b57dadac0862e3f812bc64fa1ac9e5fe5df
Judhunja <judxhunja@gmail.com>
2024-05-30 17:19:46 +0300

    flask request to post data to web server

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -2,15 +2,29 @@
 """This module contains a basic flask app"""
 
 import flask
-from flask import Flask
-
+from flask import Flask, request
+from auth import Auth
 
 app = Flask(__name__)
+AUTH = Auth()
 
 
 @app.route("/")
 def get():
+    """Return jsonified payload"""
     return flask.jsonify({"message": "Bienvenue"})
+
+
+@app.route("/users", methods=['POST'])
+def users():
+    """Send user data"""
+    email = request.form['email']
+    password = request.form['password']
+    try:
+        user = AUTH.register_user(email, password)
+        return flask.jsonify({"email": user.email, "message": "user created"})
+    except ValueError:
+        return flask.jsonify({"message": "email already registered"})
 
 
 if __name__ == "__main__":
