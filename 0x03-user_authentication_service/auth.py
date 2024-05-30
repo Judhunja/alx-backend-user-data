e1ab8de608e8ce30ec69091114b9c94b3bdd90b9
Judhunja <judxhunja@gmail.com>
2024-05-30 17:59:06 +0300

    catch all expections to return false in except block

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -35,5 +35,5 @@ class Auth:
             user = self._db.find_user_by(email=email)
             return bcrypt.checkpw(password.encode(),
                                   user.hashed_password.encode())
-        except NoResultFound:
+        except Exception:
             return False
