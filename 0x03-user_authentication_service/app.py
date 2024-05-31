0b04d9a41b670445bcedce61819c01e56bd0c903
Judhunja <judxhunja@gmail.com>
2024-05-31 17:19:52 +0300

    creating a new session for a user after login

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -2,11 +2,14 @@
 """This module contains a basic flask app"""
 
 import flask
-from flask import Flask, request
+from flask import Flask, request, session, make_response
 from auth import Auth
+import uuid
+
 
 app = Flask(__name__)
 AUTH = Auth()
+app.secret_key = b'\x996\xdb\xf5\xf0&\xbe[\xcev\xec\xe1\x95\x08KA'
 
 
 @app.route("/")
@@ -25,6 +28,25 @@ def users():
         return flask.jsonify({"email": user.email, "message": "user created"})
     except ValueError:
         return flask.jsonify({"message": "email already registered"})
+
+
+@app.route("/sessions", methods=['POST'], strict_slashes=False)
+def login():
+    """Login a user"""
+    email = request.form['email']
+    password = request.form['password']
+
+    if not AUTH.valid_login(email, password):
+        flask.abort(401)
+
+    user = AUTH._db.find_user_by(email=email)
+    session_id = str(uuid.uuid4())
+    user.session_id = session_id
+    session["session_id"] = session_id
+    resp = make_response(flask.jsonify(
+        {"email": email, "message": "logged in"}))
+    resp.set_cookie("session_id", session_id)
+    return resp
 
 
 if __name__ == "__main__":
