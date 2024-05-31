e9ecd01dfe37a8c0ed66ad33d9e04cb8bd0733e3
Judhunja <judxhunja@gmail.com>
2024-05-31 17:34:39 +0300

    destroying a user session by setting session_id to None

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -66,3 +66,7 @@ class Auth:
             return user
         except NoResultFound:
             return None
+
+    def destroy_session(self, user_id: int) -> None:
+        """Clears a user session after the user is done using it"""
+        self._db.update_user(user_id, session_id=None)
