866f667cf0ea440fca14989a1dcdfafb2eaed602
Judhunja <judxhunja@gmail.com>
2024-07-05 03:15:32 +0300

    checking whether the provided password matches the hashed password

--- 0x00-personal_data/encrypt_password.py
+++ 0x00-personal_data/encrypt_password.py
@@ -10,3 +10,9 @@ def hash_password(password: str) -> bytes:
         a byte string"""
     salt = bcrypt.gensalt()
     return bcrypt.hashpw(password.encode(), salt)
+
+
+def is_valid(hashed_password: bytes, password: str) -> bool:
+    """Checks whether a password is valid"""
+    # check the password to be tested against the hashed
+    return bcrypt.checkpw(password.encode(), hashed_password)
