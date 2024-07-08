a011c2f381c0c3387fa2ae88940586cd299c2ddf
Judhunja <judxhunja@gmail.com>
2024-07-08 19:32:40 +0300

    fix: pycodestyle validation

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -7,18 +7,17 @@ from typing import List, TypeVar
 
 
 class Auth:
-    def __init__(self):
-        """Initializes class Auth"""
-        pass
-
+    # def __init__(self):
+    #   """Initializes class Auth"""
+    #  pass
     def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
-        """"""
+        """Require auth"""
         return False
 
     def authorization_header(self, request=None) -> str:
-        """"""
+        """Authorization header"""
         return None
 
-    def current_user(self, request=None) -> TypeVar('User'):
-        """"""
+    def current_user(self, request=None) -> TypeVar("User"):
+        """Current user"""
         return None
