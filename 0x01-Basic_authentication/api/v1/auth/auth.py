dde97310d26677377affbc806f47a50dd5b0d764
Judhunja <judxhunja@gmail.com>
2024-07-09 15:27:23 +0300

    Optional type annotation return value for functions returning None

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -3,7 +3,7 @@
 
 
 from flask import request
-from typing import List, TypeVar
+from typing import List, Optional, TypeVar
 
 
 class Auth:
@@ -14,10 +14,10 @@ class Auth:
         """Require auth"""
         return False
 
-    def authorization_header(self, request=None) -> str:
+    def authorization_header(self, request=None) -> Optional[str]:
         """Authorization header"""
         return None
 
-    def current_user(self, request=None) -> TypeVar("User"):
+    def current_user(self, request=None) -> Optional[TypeVar('User')]:
         """Current user"""
         return None
