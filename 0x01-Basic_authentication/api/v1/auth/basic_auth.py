31fa8c64a8fc7f1d0b2355752f693fe3e372496a
Judhunja <judxhunja@gmail.com>
2024-07-09 20:21:21 +0300

    utf-8 decoding of base64 encoded string

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -3,6 +3,7 @@
 
 
 from .auth import Auth
+import base64
 
 
 class BasicAuth(Auth):
@@ -19,3 +20,20 @@ class BasicAuth(Auth):
         ):
             return None
         return authorization_header.lstrip("Basic ")
+
+    def decode_base64_authorization_header(
+        self,
+        base64_authorization_header: str
+    ) -> str:
+        """Decoding the base64 part of the authorization header"""
+        if (
+            base64_authorization_header is None
+            or not isinstance(base64_authorization_header, str)
+        ):
+            return None
+
+        try:
+            return base64.b64decode(
+                base64_authorization_header).decode('utf-8')
+        except Exception:
+            return None
