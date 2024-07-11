beda14270685e25053070ae5dce3605ba24a74f6
Judhunja <judxhunja@gmail.com>
2024-07-11 21:42:08 +0300

    storing a user in a dict with the key being the session_id

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -2,7 +2,22 @@
 """This module contains a class SessionAuth"""
 
 from ..auth.auth import Auth
+from uuid import uuid4
 
 
 class SessionAuth(Auth):
     """Session authentication mechanism"""
+
+    user_id_by_session_id = {}
+
+    def create_session(self, user_id: str = None) -> str:
+        """Creates a session id for a user_id"""
+        if (
+            user_id is None
+            or not isinstance(user_id, str)
+        ):
+            return None
+
+        session_id = str(uuid4())
+        self.user_id_by_session_id[session_id] = user_id
+        return session_id
